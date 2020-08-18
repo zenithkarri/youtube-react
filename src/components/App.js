d6ebbad22e73e155a04ef882801673acd1a72763
Zenith Karri <tripshire@gmail.com>
2020-08-18 08:23:04 +0530

    Revert "Add styling, and embed Video"
    This reverts commit e5e61dc3e5e05092ec1eea46cd67567d2b1d1394.

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,6 @@ import VideoDetail from './VideoDetail';
 class App extends React.Component{
     state={ videos: [], selectedVideo: null };
     
-    componentDidMount(){
-        this.onTermSubmit('buildings');
-    }
-
     onTermSubmit = async (term) => {
         const KEY = 'AIzaSyDaA7lHgoDgPmbPay_vTNKE34nefVDCkX8';
         const response = await youtube.get('/search', {
@@ -22,7 +18,7 @@ class App extends React.Component{
                 type: 'video'
             }
         });
-        this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
+        this.setState({ videos: response.data.items });
     };
     
     onVideoSelect = (video) => {
@@ -33,20 +29,8 @@ class App extends React.Component{
         return (
             <div className="ui container">
                 <SearchBar onTermSubmit={this.onTermSubmit} />
-                
-                <div className="ui grid" >
-                    <div className="ui row" >
-                        <div className="eleven wide column" >
-                            <VideoDetail video={this.state.selectedVideo} />
-                        </div>
-                        <div className="five wide column" >
-                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
-                        </div>
-                        
-                    </div>
-                    
-                </div>
-                
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
             </div>
             
         );
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -5,11 +5,11 @@ const VideoItem = (props) => {
     return (
         // onVideoSelect is a callback function that communicates to the grandparent: App component 
         <div onClick={() => props.onVideoSelect(props.video)} className="video-item item" >
-            <img alt={props.video.snippet.title} className="ui image" src={ props.video.snippet.thumbnails.medium.url } />
+            <img className="ui image" src={ props.video.snippet.thumbnails.medium.url } />
             <div className="content">
-                <div className="header">
+                <a className="header">
                     { props.video.snippet.title }
-                </div>
+                </a>
             </div>
         </div>
     );
